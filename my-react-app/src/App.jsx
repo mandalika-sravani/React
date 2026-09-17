e30ff866761097aa502e0bb304095b81df93b26e
sravani-mandalika <sravanimadalika@gmail.com>
2026-09-17 16:40:36 +0530

    added CSS Styles

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,17 +2,20 @@ import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
 import Food from "./Food.jsx";
 import Card from "./Card.jsx";
+import Button from "./Button/Button.jsx";
 
 function App() {
 
   return (
     <>
         {/*<Header/>
-        <Food/> */}
+        <Food/> 
         <Card />
         <Card />
         <Card />
-        {/*<Footer/>*/}
+        <Footer/>*/}
+
+        <Button />
     </>
     
   );
